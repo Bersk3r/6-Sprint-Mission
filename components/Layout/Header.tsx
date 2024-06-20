e4da877796f9b009378cdd849ff538de39558580
Bersk3r <kiscwook@gmail.com>
2024-06-21 02:13:52 +0900

    ♻️ Refactor: user 정보와 accessToken이 존재할 때에만 프로필 이미지 표시
    user 정보와 accessToken이 존재할 때에만 프로필 이미지를 표시하도록 변경

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 
 import Logo from "../../src/assets/images/logo/logo.svg";
 import style from "./Header.module.scss";
@@ -7,7 +6,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import ProfileImage from "../../src/assets/images/ui/ic_profile.svg";
-import ProfileImage from "../../src/assets/images/ui/ic_profile.svg";
+import { useAuth } from "../../contexts/AuthProvider";
 
 const menuItems = [
   { id: "item1", name: "자유게시판", path: "/board" },
@@ -16,6 +15,7 @@ const menuItems = [
 
 export default function Header() {
   const router = useRouter();
+  const { user } = useAuth(false);
   const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
@@ -53,7 +53,7 @@ export default function Header() {
           </ul>
         </nav>
       </div>
-      {isAuth ? (
+      {user && isAuth ? (
         <ProfileImage />
       ) : (
         <Link href="/signin" className={style.login_link}>
